fcdf5f53bb4642461845fb6f2536d316e219c590
thamarai <thamaraiselvi9500@gmail.com>
2024-07-17 09:32:44 +0200

    change-component-name

--- Movie-Search-App/src/App.jsx
+++ Movie-Search-App/src/App.jsx
@@ -10,9 +10,9 @@ function App() {
 
   return (
     <>
-      {/* <Home/>*/}
-      <Samplefectch/> 
-      {/* <Search/> */}
+       <Home/>
+      {/* <Samplefectch/> 
+      <Search/>  */}
     </>
   );
 }
